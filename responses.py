878097741c674677e567b2cefe37193988ff9674
Mert Ali Özçelik <bakruux@gmail.com>
2023-01-27 22:48:56 +0300

    Update responses.py

--- responses.py
+++ responses.py
@@ -15,16 +15,14 @@ def handle_response(message) -> str:
     if p_message == 'ig':
         return 'Tatlı rüyalar!'
 
-
     if p_message == 'la li lu le lo':
         return 'Damn The Patriots.'
-
 
     if p_message == '.yayın':
         return 'Yayınlar bu adreslerde! \nhttps://www.twitch.tv/bakruu \n https://www.youtube.com/bakruu'
 
     if p_message == '.help':
-        return 'Yardım için zamanyolcusu.net adresine gidebilirsin! İşte bazı komutlar; \n.yayın \n.minecraft \n.discord \n .davet \n.steam \n.help'
+        return 'Yardım için zamanyolcusu.net adresine gidebilirsin! İşte bazı komutlar; \n.yayın \n.minecraft \n.discord \n .davet \n.steam \n.help \n.d20 (20 lik zar atar.)'
 
     if p_message == '.minecraft':
         return 'Server ip: "193.122.53.126" \nDomain: zamanyolcusu.net'
